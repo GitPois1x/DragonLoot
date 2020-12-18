e9dd2ef1047b48aadcd089b9a221ddd519df89b0
Globox1997 <mitschke11@web.de>
2020-12-18 21:25:17 +0100

    added knockback resistance

--- src/main/java/net/dragonloot/item/DragonArmor.java
+++ src/main/java/net/dragonloot/item/DragonArmor.java
@@ -1,14 +1,54 @@
 package net.dragonloot.item;
 
+import java.util.UUID;
+
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.ImmutableMultimap.Builder;
+
+import net.minecraft.block.DispenserBlock;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
 
 public class DragonArmor extends ArmorItem {
+    private static final UUID[] MODIFIERS = new UUID[] { UUID.fromString("845DB27C-C624-495F-8C9F-6020A9A58B6B"),
+            UUID.fromString("D8499B04-0E66-4726-AB29-64469D734E0D"),
+            UUID.fromString("9F3D476D-C118-4544-8365-64846904B48E"),
+            UUID.fromString("2AD3F246-FEE1-4E67-B886-69FD380BB150") };
+    private final Multimap<EntityAttribute, EntityAttributeModifier> attributeModifiers;
+    private final int protection;
+    private final float toughness;
+    protected final float knockbackResistance;
+    protected final EquipmentSlot slot;
+    protected final ArmorMaterial type;
 
     public DragonArmor(ArmorMaterial material, EquipmentSlot slot, Settings settings) {
         super(material, slot, settings);
+        this.type = material;
+        this.slot = slot;
+        this.protection = material.getProtectionAmount(slot);
+        this.toughness = material.getToughness();
+        this.knockbackResistance = material.getKnockbackResistance();
+        DispenserBlock.registerBehavior(this, DISPENSER_BEHAVIOR);
+        Builder<EntityAttribute, EntityAttributeModifier> builder = ImmutableMultimap.builder();
+        UUID uUID = MODIFIERS[slot.getEntitySlotId()];
+        builder.put(EntityAttributes.GENERIC_ARMOR, new EntityAttributeModifier(uUID, "Armor modifier",
+                (double) this.protection, EntityAttributeModifier.Operation.ADDITION));
+        builder.put(EntityAttributes.GENERIC_ARMOR_TOUGHNESS, new EntityAttributeModifier(uUID, "Armor toughness",
+                (double) this.toughness, EntityAttributeModifier.Operation.ADDITION));
+        builder.put(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE,
+                new EntityAttributeModifier(uUID, "Armor knockback resistance", (double) this.knockbackResistance / 10D,
+                        EntityAttributeModifier.Operation.ADDITION));
+        this.attributeModifiers = builder.build();
+    }
 
+    @Override
+    public Multimap<EntityAttribute, EntityAttributeModifier> getAttributeModifiers(EquipmentSlot slot) {
+        return slot == this.slot ? this.attributeModifiers : super.getAttributeModifiers(slot);
     }
 
 }
