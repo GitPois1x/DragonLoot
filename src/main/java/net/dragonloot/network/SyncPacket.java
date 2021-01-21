766620e1cf99ef028406e61f138be9f03b023db0
Globox1997 <mitschke11@web.de>
2021-01-21 12:09:15 +0100

    updated to new network api

--- src/main/java/net/dragonloot/network/SyncPacket.java
+++ src/main/java/net/dragonloot/network/SyncPacket.java
@@ -1,6 +1,7 @@
 package net.dragonloot.network;
 
 import net.dragonloot.access.DragonAnvilInterface;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.screen.AnvilScreenHandler;
@@ -10,12 +11,12 @@ public class SyncPacket {
     public static final Identifier ANVIL_SYNC_PACKET = new Identifier("dragonloot", "dragon_anvil_sync");
 
     public static void init() {
-        ClientSidePacketRegistry.INSTANCE.register(ANVIL_SYNC_PACKET, (context, buffer) -> {
+        ClientPlayNetworking.registerGlobalReceiver(ANVIL_SYNC_PACKET, (client, handler, buffer, responseSender) -> {
             int entityId = buffer.readInt();
             String blockString = buffer.readString();
-            context.getTaskQueue().execute(() -> {
-                if (context.getPlayer().world.getEntityById(entityId) != null) {
-                    PlayerEntity player = (PlayerEntity) context.getPlayer().world.getEntityById(entityId);
+            client.execute(() -> {
+                if (client.player.world.getEntityById(entityId) != null) {
+                    PlayerEntity player = (PlayerEntity) client.player.world.getEntityById(entityId);
                     if (player.currentScreenHandler instanceof AnvilScreenHandler) {
                         ((DragonAnvilInterface) player.currentScreenHandler).setDragonAnvil(blockString);
                     }
@@ -23,6 +24,20 @@ public class SyncPacket {
                 }
             });
         });
+
+        // ClientSidePacketRegistry.INSTANCE.register(ANVIL_SYNC_PACKET, (context, buffer) -> {
+        //     int entityId = buffer.readInt();
+        //     String blockString = buffer.readString();
+        //     context.getTaskQueue().execute(() -> {
+        //         if (context.getPlayer().world.getEntityById(entityId) != null) {
+        //             PlayerEntity player = (PlayerEntity) context.getPlayer().world.getEntityById(entityId);
+        //             if (player.currentScreenHandler instanceof AnvilScreenHandler) {
+        //                 ((DragonAnvilInterface) player.currentScreenHandler).setDragonAnvil(blockString);
+        //             }
+
+        //         }
+        //     });
+        // });
     }
 
 }
