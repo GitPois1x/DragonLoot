e63a882480d3ae8f639bb96d7a92d14a136669f5
Globox1997 <mitschke11@web.de>
2021-05-14 09:58:14 +0200

    silence log

--- src/main/java/net/dragonloot/mixin/PlayerInventoryMixin.java
+++ src/main/java/net/dragonloot/mixin/PlayerInventoryMixin.java
@@ -2,6 +2,7 @@ package net.dragonloot.mixin;
 
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -23,6 +24,7 @@ public class PlayerInventoryMixin {
     public final PlayerEntity player;
     @Shadow
     @Final
+    @Mutable
     public final DefaultedList<ItemStack> armor;
 
     public PlayerInventoryMixin(PlayerEntity player) {
