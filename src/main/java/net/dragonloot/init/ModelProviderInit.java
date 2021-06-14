37a248b3af323c5f088fd1696c429cc656e0d9af
Globox1997 <mitschke11@web.de>
2021-06-14 16:36:56 +0200

    model fix

--- src/main/java/net/dragonloot/init/ModelProviderInit.java
+++ src/main/java/net/dragonloot/init/ModelProviderInit.java
@@ -7,45 +7,24 @@ import net.minecraft.util.Identifier;
 
 public class ModelProviderInit {
 
-        public static void init() {
-                FabricModelPredicateProviderRegistry.register(ItemInit.DRAGON_BOW_ITEM, new Identifier("pull"), (
-                                itemStack, clientWorld, livingEntity,
-                                seed) -> livingEntity != null && livingEntity.getActiveItem() != itemStack ? 0.0F
-                                                : (itemStack.getMaxUseTime() - livingEntity.getItemUseTimeLeft())
-                                                                / 20.0F);
-                FabricModelPredicateProviderRegistry.register(ItemInit.DRAGON_BOW_ITEM, new Identifier("pulling"),
-                                (itemStack, clientWorld, livingEntity,
-                                                seed) -> livingEntity != null && livingEntity.isUsingItem()
-                                                                && livingEntity.getActiveItem() == itemStack ? 1.0F
-                                                                                : 0.0F);
-
-                FabricModelPredicateProviderRegistry.register(ItemInit.DRAGON_CROSSBOW_ITEM, new Identifier("pull"),
-                                (itemStack, clientWorld, livingEntity, seed) -> {
-                                        if (livingEntity == null) {
-                                                return 0;
-                                        }
-                                        return CrossbowItem.isCharged(itemStack) ? 0.0F
-                                                        : (float) (itemStack.getMaxUseTime()
-                                                                        - livingEntity.getItemUseTimeLeft())
-                                                                        / (float) CrossbowItem.getPullTime(itemStack);
-                                });
-                FabricModelPredicateProviderRegistry.register(ItemInit.DRAGON_CROSSBOW_ITEM, new Identifier("pulling"),
-                                (itemStack, clientWorld1, livingEntity,
-                                                seed) -> livingEntity != null && livingEntity.isUsingItem()
-                                                                && livingEntity.getActiveItem() == itemStack
-                                                                && !CrossbowItem.isCharged(itemStack) ? 1.0F : 0.0F);
-                FabricModelPredicateProviderRegistry.register(ItemInit.DRAGON_CROSSBOW_ITEM, new Identifier("charged"),
-                                (itemStack, clientWorld, livingEntity, seed) -> livingEntity != null
-                                                && CrossbowItem.isCharged(itemStack) ? 1.0F : 0.0F);
-                FabricModelPredicateProviderRegistry.register(ItemInit.DRAGON_CROSSBOW_ITEM, new Identifier("firework"),
-                                (itemStack, clientWorld, livingEntity, seed) -> livingEntity != null
-                                                && CrossbowItem.isCharged(itemStack)
-                                                && CrossbowItem.hasProjectile(itemStack, Items.FIREWORK_ROCKET) ? 1.0F
-                                                                : 0.0F);
-                FabricModelPredicateProviderRegistry.register(ItemInit.DRAGON_TRIDENT_ITEM, new Identifier("throwing"),
-                                (itemStack, clientWorld, livingEntity,
-                                                seed) -> livingEntity != null && livingEntity.isUsingItem()
-                                                                && livingEntity.getActiveItem() == itemStack ? 1.0F
-                                                                                : 0.0F);
-        }
+      public static void init() {
+            FabricModelPredicateProviderRegistry.register(ItemInit.DRAGON_BOW_ITEM, new Identifier("pull"), (stack, world, entity, seed) -> {
+                  if (entity == null) {
+                        return 0.0F;
+                  } else {
+                        return entity.getActiveItem() != stack ? 0.0F : (float) (stack.getMaxUseTime() - entity.getItemUseTimeLeft()) / 20.0F;
+                  }
+            });
+            FabricModelPredicateProviderRegistry.register(ItemInit.DRAGON_BOW_ITEM, new Identifier("pulling"), (itemStack, clientWorld, livingEntity, seed) -> livingEntity != null && livingEntity.isUsingItem() && livingEntity.getActiveItem() == itemStack ? 1.0F : 0.0F);
+            FabricModelPredicateProviderRegistry.register(ItemInit.DRAGON_CROSSBOW_ITEM, new Identifier("pull"), (itemStack, clientWorld, livingEntity, seed) -> {
+                  if (livingEntity == null) {
+                        return 0;
+                  } else
+                        return CrossbowItem.isCharged(itemStack) ? 0.0F : (float) (itemStack.getMaxUseTime() - livingEntity.getItemUseTimeLeft()) / (float) CrossbowItem.getPullTime(itemStack);
+            });
+            FabricModelPredicateProviderRegistry.register(ItemInit.DRAGON_CROSSBOW_ITEM, new Identifier("pulling"), (itemStack, clientWorld1, livingEntity, seed) -> livingEntity != null && livingEntity.isUsingItem() && livingEntity.getActiveItem() == itemStack && !CrossbowItem.isCharged(itemStack) ? 1.0F : 0.0F);
+            FabricModelPredicateProviderRegistry.register(ItemInit.DRAGON_CROSSBOW_ITEM, new Identifier("charged"), (itemStack, clientWorld, livingEntity, seed) -> livingEntity != null && CrossbowItem.isCharged(itemStack) ? 1.0F : 0.0F);
+            FabricModelPredicateProviderRegistry.register(ItemInit.DRAGON_CROSSBOW_ITEM, new Identifier("firework"), (itemStack, clientWorld, livingEntity, seed) -> livingEntity != null && CrossbowItem.isCharged(itemStack) && CrossbowItem.hasProjectile(itemStack, Items.FIREWORK_ROCKET) ? 1.0F : 0.0F);
+            FabricModelPredicateProviderRegistry.register(ItemInit.DRAGON_TRIDENT_ITEM, new Identifier("throwing"), (itemStack, clientWorld, livingEntity, seed) -> livingEntity != null && livingEntity.isUsingItem() && livingEntity.getActiveItem() == itemStack ? 1.0F : 0.0F);
+      }
 }
