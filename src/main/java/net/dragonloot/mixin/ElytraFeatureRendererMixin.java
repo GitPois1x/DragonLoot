e2374aa6332bb733bc07249d3378419f7ab6da60
Globox1997 <mitschke11@web.de>
2021-08-24 20:20:24 +0200

    removed unneccessary

--- src/main/java/net/dragonloot/mixin/ElytraFeatureRendererMixin.java
+++ src/main/java/net/dragonloot/mixin/ElytraFeatureRendererMixin.java
@@ -9,12 +9,10 @@ import net.dragonloot.entity.model.DragonElytraEntityModel;
 import net.dragonloot.init.ItemInit;
 import net.fabricmc.api.Environment;
 import net.fabricmc.api.EnvType;
-import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.entity.PlayerModelPart;
 import net.minecraft.client.render.entity.feature.ElytraFeatureRenderer;
 import net.minecraft.client.render.entity.feature.FeatureRenderer;
 import net.minecraft.client.render.entity.feature.FeatureRendererContext;
@@ -40,25 +38,11 @@ public abstract class ElytraFeatureRendererMixin<T extends LivingEntity, M exten
     public void render(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, T livingEntity, float f, float g, float h, float j, float k, float l, CallbackInfo info) {
         ItemStack itemStack = livingEntity.getEquippedStack(EquipmentSlot.CHEST);
         if (itemStack.getItem() == ItemInit.UPGRADED_DRAGON_CHESTPLATE) {
-            Identifier identifier4;
-            if (livingEntity instanceof AbstractClientPlayerEntity) {
-                AbstractClientPlayerEntity abstractClientPlayerEntity = (AbstractClientPlayerEntity) livingEntity;
-                if (abstractClientPlayerEntity.canRenderElytraTexture() && abstractClientPlayerEntity.getElytraTexture() != null) {
-                    identifier4 = abstractClientPlayerEntity.getElytraTexture();
-                } else if (abstractClientPlayerEntity.canRenderCapeTexture() && abstractClientPlayerEntity.getCapeTexture() != null && abstractClientPlayerEntity.isPartVisible(PlayerModelPart.CAPE)) {
-                    identifier4 = abstractClientPlayerEntity.getCapeTexture();
-                } else {
-                    identifier4 = DRAGON_ELYTRA_TEXTURE;
-                }
-            } else {
-                identifier4 = DRAGON_ELYTRA_TEXTURE;
-            }
-
             matrixStack.push();
             matrixStack.translate(0.0D, 0.0D, 0.02D);
             this.getContextModel().copyStateTo(this.dragonElytraModel);
             this.dragonElytraModel.setAngles(livingEntity, f, g, j, k, l);
-            VertexConsumer vertexConsumer = ItemRenderer.getArmorGlintConsumer(vertexConsumerProvider, RenderLayer.getArmorCutoutNoCull(identifier4), false, itemStack.hasGlint());
+            VertexConsumer vertexConsumer = ItemRenderer.getArmorGlintConsumer(vertexConsumerProvider, RenderLayer.getArmorCutoutNoCull(DRAGON_ELYTRA_TEXTURE), false, itemStack.hasGlint());
             this.dragonElytraModel.render(matrixStack, vertexConsumer, i, OverlayTexture.DEFAULT_UV, 1.0F, 1.0F, 1.0F, 1.0F);
             matrixStack.pop();
             info.cancel();
