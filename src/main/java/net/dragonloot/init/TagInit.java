48c4f389a0f7db88c794948b90448cc1338fce4b
Globox1997 <mitschke11@web.de>
2022-03-06 12:12:08 +0100

    updated dependencies

--- src/main/java/net/dragonloot/init/TagInit.java
+++ src/main/java/net/dragonloot/init/TagInit.java
@@ -1,12 +1,12 @@
 package net.dragonloot.init;
 
-import net.fabricmc.fabric.api.tag.TagRegistry;
 import net.minecraft.item.Item;
-import net.minecraft.tag.Tag;
+import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class TagInit {
-    public static final Tag<Item> NOT_DESTROYED_BY_EXPLOSION = TagRegistry.item(new Identifier("dragonloot", "explosion_resistant"));
+    public static final TagKey<Item> NOT_DESTROYED_BY_EXPLOSION = TagKey.of(Registry.ITEM_KEY, new Identifier("dragonloot", "explosion_resistant"));
 
     public static void init() {
 
--- src/main/java/net/dragonloot/item/DragonTridentItem.java
+++ src/main/java/net/dragonloot/item/DragonTridentItem.java
@@ -68,7 +68,7 @@ public class DragonTridentItem extends TridentItem {
                         k *= n / m;
                         l *= n / m;
                         playerEntity.addVelocity(h, k, l);
-                        playerEntity.setRiptideTicks(20);
+                        playerEntity.useRiptide(20);
                         if (playerEntity.isOnGround()) {
                             float o = 1.1999999F;
                             playerEntity.move(MovementType.SELF, new Vec3d(0.0D, o, 0.0D));
