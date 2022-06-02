bdba0bf65d2b2e29fa963af06e4d37b54241e2c6
Globox1997 <mitschke11@web.de>
2022-06-02 12:08:43 +0200

    flush

--- src/main/java/net/dragonloot/item/DragonArmorMaterial.java
+++ src/main/java/net/dragonloot/item/DragonArmorMaterial.java
@@ -2,8 +2,6 @@ package net.dragonloot.item;
 
 import net.dragonloot.init.ConfigInit;
 import net.dragonloot.init.ItemInit;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.recipe.Ingredient;
