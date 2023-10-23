cb96a2cb3932e745d90085003a03800c20b032f9
TheWinABagel <gogglebobby@gmail.com>
2023-10-23 14:45:24 -0500

    Replace bow + crossbow mixins with Projectile Damage attribute

--- src/main/java/net/dragonloot/item/DragonCrossbowItem.java
+++ src/main/java/net/dragonloot/item/DragonCrossbowItem.java
@@ -3,11 +3,13 @@ package net.dragonloot.item;
 import net.dragonloot.init.ItemInit;
 import net.minecraft.item.CrossbowItem;
 import net.minecraft.item.ItemStack;
+import net.projectile_damage.api.IProjectileWeapon;
 
 public class DragonCrossbowItem extends CrossbowItem {
 
     public DragonCrossbowItem(Settings settings) {
         super(settings);
+        ((IProjectileWeapon) this).setProjectileDamage(12.25f);
     }
 
     @Override
